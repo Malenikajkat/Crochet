b83e783c6419b47bc9ba7f047fd12abca799d9c6
malenikajkat <malenikajkat@mail.ru>
2024-07-06 15:55:45 +0300

    Раздел видео

--- app/src/main/java/com/malenikajkat/crochets/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/malenikajkat/crochets/ui/home/HomeViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class HomeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "Игрушки вязанные крючком"
     }
     val text: LiveData<String> = _text
 }
--- app/src/main/java/com/malenikajkat/crochets/ui/schemes/SchemesFragment.kt
+++ app/src/main/java/com/malenikajkat/crochets/ui/schemes/SchemesFragment.kt
@@ -1,4 +1,4 @@
-package com.malenikajkat.crochets.ui.gallery
+package com.malenikajkat.crochets.ui.schemes
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.malenikajkat.crochets.databinding.FragmentGalleryBinding
+import com.malenikajkat.crochets.databinding.FragmentSchemesBinding
 
-class GalleryFragment : Fragment() {
+class SchemesFragment : Fragment() {
 
-    private var _binding: FragmentGalleryBinding? = null
+    private var _binding: FragmentSchemesBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class GalleryFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val galleryViewModel =
-            ViewModelProvider(this).get(GalleryViewModel::class.java)
+        val schemesViewModel =
+            ViewModelProvider(this).get(SchemesViewModel::class.java)
 
-        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
+        _binding = FragmentSchemesBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textGallery
-        galleryViewModel.text.observe(viewLifecycleOwner) {
+        schemesViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
